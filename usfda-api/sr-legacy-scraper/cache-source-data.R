1d16829546b63ebdb9ed8ab5dfe92d9af8719986
rsubramanyam2994 <subramanyam@systeminsights.com>
2021-12-10 01:01:53 +0530

    Scraped cup to grams from usfda

--- usfda-api/sr-legacy-scraper/cache-source-data.R
+++ usfda-api/sr-legacy-scraper/cache-source-data.R
@@ -2,14 +2,47 @@ library(stringr)
 library(plyr)
 library(dplyr)
 
-sr_legacy_data <- jsonlite::fromJSON(readLines(paste0("/Users/subramanyam/subbu/food-project/data/usfda/FoodData_Central_sr_legacy_food_json_2021-10-28.json")))
+sr_legacy_data <- jsonlite::fromJSON(readLines(paste0("/Users/subramanyam/Downloads/FoodData_Central_sr_legacy_food_json_2021-10-28.json")))
 
 metadata <- data.frame(
   s_no = seq(1:7793),
   food_category = sr_legacy_data$SRLegacyFoods$foodCategory$description,
   ndb_number = sr_legacy_data$SRLegacyFoods$ndbNumber,
-  food_description = sr_legacy_data$SRLegacyFoods$description
+  food_description = sr_legacy_data$SRLegacyFoods$description,
+  stringsAsFactors = FALSE
 )
+
+# j <- 1
+# food_portions_raw <- ldply(sr_legacy_data$SRLegacyFoods$foodPortions, function(x) {
+#   
+#   print(j)
+#   # x = sr_legacy_data$SRLegacyFoods$foodPortions[[96]]
+#   df = NA
+#   if ("measureUnit" %in% names(x)) {
+#     df = x %>% select(-measureUnit) %>% cbind(x$measureUnit)
+#   } else {
+#     df = x
+#   }
+#   
+#   if(nrow(df) == 0) {
+#     df = data.frame(food_description = metadata$food_description[j], ndb_number = metadata$ndb_number[j],
+#                     food_category = metadata$food_category[j], missing = TRUE)
+#     j <<- j + 1
+#     return(df)
+#   }
+#   
+#   df$food_description = metadata$food_description[j]
+#   df$ndb_number = metadata$ndb_number[j]
+#   df$food_category = metadata$food_category[j]
+#   df$missing = FALSE
+#   
+#   j <<- j + 1
+#   
+#   return(df)
+#   
+# })
+
+saveRDS(food_portions_raw, "/Users/subramanyam/subbu/food-project/cache/food_portions_raw")
 
 # TODO: Use given conversion factors instead of 4:4:9
 # i <- 0
@@ -61,5 +94,3 @@ saveRDS(unique_foods, "/Users/subramanyam/subbu/food-project/cache/unique_foods"
 non_saturated_fat_micro_codes <- c("617", "618", "619", "621", "625", "626", "629", "631", "687", "697", "672", "685", "689", "620", "627", "628", "630", "673", "858", "671", "857", "851", "853", "670", "674", "675", "676", "852", "856", "666", "855", "859")
 
 non_saturated_fats <- unique_nutrients %>% filter(nutrient_number %in% non_saturated_fat_micro_codes) %>% arrange(nutrient_number)
-
-non_saturated_fats_common_name_mapping <- 
