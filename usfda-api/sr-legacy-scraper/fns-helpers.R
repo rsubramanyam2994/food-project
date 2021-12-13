b85b4064adcee5d789ef3d0cd2896bfc12a25236
rsubramanyam2994 <subramanyam@systeminsights.com>
2021-12-13 10:40:59 +0530

    Recipe nutrition calculator first draft implementation

--- usfda-api/sr-legacy-scraper/fns-helpers.R
+++ usfda-api/sr-legacy-scraper/fns-helpers.R
@@ -143,8 +143,8 @@ read_foundation_food_data <- function(usfda_data) {
 
 get_measured_nutrients <- function() {
   nutrient_codes_to_extract_list <- jsonlite::fromJSON("/Users/subramanyam/subbu/food-project/data/usfda-mapping/nutrient-code-mapping.json") %>% unlist(., recursive = TRUE)
-  nutrient_codes_df <- data.frame(nutrient_code = nutrient_codes_to_extract_list, row.names = NULL) %>% 
-    mutate(nutrient_name = names(nutrient_codes_to_extract_list)) %>% filter(nutrient_code != "missing")
+  nutrient_codes_df <- data.frame(nutrient_number = nutrient_codes_to_extract_list %>% as.character, row.names = NULL) %>% 
+    mutate(nutrient_name = names(nutrient_codes_to_extract_list)) %>% filter(nutrient_number != "missing")
   
   return(nutrient_codes_df)
   
@@ -160,31 +160,83 @@ split_quantity <- function(df) {
 get_gram_multiplication_factor <- function(recipe_with_ndb_mapping, ingredient_portions) {
   relevant_food_portions <- ingredient_portions %>% filter(ndb_number %in% recipe_with_ndb_mapping$ndb_number)
   
-  ddply(recipe_with_ndb_mapping, c("ndb_number"), function(df) {
-    browser()
+  output <- ddply(recipe_with_ndb_mapping, c("ndb_number"), function(df) {
+
+    print(str_interp("Computing factor for ${df$ingredient}"))
     
     from_unit = df$unit
     from_amount = df$amount
-    
-    conversion_factor = NA
     
     if (from_unit == "g" || from_unit == "grams" || from_unit == "gram") {
       return(data.frame(ndb_number = df$ndb_number, mult_factor = from_amount / 100))
     }
     
+    available_to_units = relevant_food_portions %>% filter(ndb_number == df$ndb_number)
+    
     if (from_unit == "tsp") {
-      # if tsp is available in rhs, return corresponding gram_weight / 100
-      # if tbsp is available, return corresponding gram_weight / 100 * (tbsp / tsp conversion factor)
-      # if cup is available, return corresponding gram_weight / 100 * (cup / tsp conversion factor)
+      if ("tsp" %in% available_to_units$modifier) {
+        mult_factor = (available_to_units[available_to_units$modifier == "tsp", "gram_weight"] * from_amount) / 100
+        return(data.frame(ndb_number = df$ndb_number, mult_factor = mult_factor))
+      }
       
-      # unavailable
+      if ("tbsp" %in% available_to_units$modifier) {
+        mult_factor = (available_to_units[available_to_units$modifier == "tbsp", "gram_weight"] * from_amount * 0.33) / 100
+        return(data.frame(ndb_number = df$ndb_number, mult_factor = mult_factor))
+      }
       
+      if ("cup" %in% available_to_units$modifier) {
+        mult_factor = (available_to_units[available_to_units$modifier == "cup", "gram_weight"] * from_amount * 0.0208333) / 100
+        return(data.frame(ndb_number = df$ndb_number, mult_factor = mult_factor))
+      }
+      
+      stop(str_interp("No available to unit for ${df$ingredient}"))
     }
+    
+    if (from_unit == "tbsp") {
+      if ("tbsp" %in% available_to_units$modifier) {
+        mult_factor = (available_to_units[available_to_units$modifier == "tbsp", "gram_weight"] * from_amount) / 100
+        return(data.frame(ndb_number = df$ndb_number, mult_factor = mult_factor))
+      }
+      
+      if ("tsp" %in% available_to_units$modifier) {
+        mult_factor = (available_to_units[available_to_units$modifier == "tsp", "gram_weight"] * from_amount * 3) / 100
+        return(data.frame(ndb_number = df$ndb_number, mult_factor = mult_factor))
+      }
+      
+      if ("cup" %in% available_to_units$modifier) {
+        mult_factor = (available_to_units[available_to_units$modifier == "cup", "gram_weight"] * from_amount * 0.0625) / 100
+        return(data.frame(ndb_number = df$ndb_number, mult_factor = mult_factor))
+      }
+      
+      stop(str_interp("No available to unit for ${df$ingredient}"))
+    }
+    
+    if (from_unit == "cup") {
+      if ("cup" %in% available_to_units$modifier) {
+        mult_factor = (available_to_units[available_to_units$modifier == "cup", "gram_weight"] * from_amount) / 100
+        return(data.frame(ndb_number = df$ndb_number, mult_factor = mult_factor))
+      }
+      
+      if ("tbsp" %in% available_to_units$modifier) {
+        mult_factor = (available_to_units[available_to_units$modifier == "tbsp", "gram_weight"] * from_amount * 16) / 100
+        return(data.frame(ndb_number = df$ndb_number, mult_factor = mult_factor))
+      }
+      
+      if ("tsp" %in% available_to_units$modifier) {
+        mult_factor = (available_to_units[available_to_units$modifier == "tsp", "gram_weight"] * from_amount * 48) / 100
+        return(data.frame(ndb_number = df$ndb_number, mult_factor = mult_factor))
+      }
+      
+      stop(str_interp("No available to unit for ${df$ingredient}"))
+    }
+    
+    stop("Invalid from unit for ${df$ingredient}")
     
     # same for tbsp and cup
     
-    
-    
   })
+  
+
+  return(output)
   
 }
--- usfda-api/sr-legacy-scraper/recipe-nutrition-calculator.R
+++ usfda-api/sr-legacy-scraper/recipe-nutrition-calculator.R
@@ -1,14 +1,23 @@
 ingredient_portions <- readRDS("/Users/subramanyam/subbu/food-project/cache/food_portions")
 ingredient_ndb_mapping <- readRDS("/Users/subramanyam/subbu/food-project/cache/food_ndb_mapping") 
 ingredient_nutrition_info <- readRDS("/Users/subramanyam/subbu/food-project/cache/foundation_foods_df")
+measureable_nutrients <- get_measured_nutrients()
 
-poha <- data.frame(
+recipe <- data.frame(
   ingredient = c("sugar", "salt", "turmeric", "white-rice", "lemon-juice", "groundnut-oil", "potato", "groundnut"),
   quantity = c("1 tsp", "0.5 tsp", "0.25 tsp", "1 cup", "1 tbsp", "2 tsp", "50 grams", "1 tbsp")
 )
 
-recipe_with_ndb_mapping = merge(poha, ingredient_ndb_mapping, by.x = "ingredient", by.y = "food_name") %>% split_quantity
-gram_multiplication_factors <- get_gram_multiplication_factor(recipe_with_ndb_mapping, ingredient_portions)
+recipe <- data.frame(
+  ingredient = c("vermicelli", "peas", "extra-virgin-olive-oil", "salt"),
+  quantity = c("1 cup", "1 tbsp", "1 tbsp", "1.5 tsp")
+)
+
+recipe_with_ndb_mapping = merge(recipe, ingredient_ndb_mapping, by.x = "ingredient", by.y = "food_name") %>% split_quantity
+gram_multiplication_factors <- get_gram_multiplication_factor(recipe_with_ndb_mapping, ingredient_portions) %>% merge(., recipe_with_ndb_mapping, by = c("ndb_number"))
+
+recipe_nutrition_info <- ingredient_nutrition_info %>% filter(ndb_number %in% gram_multiplication_factors$ndb_number) %>% merge(., gram_multiplication_factors %>% select(-c(amount, unit)), by = c("ndb_number")) %>% mutate(nutrient_number = as.character(nutrient_number)) %>% merge(., measureable_nutrients, by = c("nutrient_number")) %>% 
+  transmute(name = ingredient, nutrient = nutrient_name.x, path = nutrient_name.y, amount = amount * mult_factor, unit = unit)
 
 # filter for relevant ndb_numbers in ingredient_nutrition_info, merge based on ndb_number, multiply amount by mult_factor
 # group by nutrient_codes, nutrient_name, merge with measureable_nutrients <- get_measured_nutrients(), filter for macros / micros using search to see summary
